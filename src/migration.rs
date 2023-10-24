5d3f9554285c7f0df1abd6ece7c9ba0e93942e5e
Marian Vanderka <vanderka.marian@gmail.com>
2023-10-24 14:15:23 +0200

    remove migration, keep version upgrade only

--- src/migration.rs
+++ src/migration.rs
@@ -24,49 +24,11 @@ pub mod v1 {
 
 	use sp_core::Get;
 
-	#[frame_support::storage_alias]
-	pub(super) type UnassociatedContributions<T: Config> =
-		StorageMap<Pallet<T>, Blake2_128Concat, <T as Config>::RelayChainAccountId, RewardInfo<T>>;
-
-	#[frame_support::storage_alias]
-	pub(super) type AccountsPayable<T: Config> = StorageMap<
-		Pallet<T>,
-		Blake2_128Concat,
-		<T as frame_system::Config>::AccountId,
-		RewardInfo<T>,
-	>;
-
-	#[frame_support::storage_alias]
-	pub type CrowdloanAllocation<T: Config> = StorageValue<Pallet<T>, BalanceOf<T>, ValueQuery>;
-
-	#[frame_support::storage_alias]
-	pub type InitializedRewardAmount<T: Config> = StorageValue<Pallet<T>, BalanceOf<T>, ValueQuery>;
-
-	#[frame_support::storage_alias]
-	pub type TotalContributors<T: Config> = StorageValue<Pallet<T>, u32, ValueQuery>;
-
-	#[frame_support::storage_alias]
-	pub type ClaimedRelayChainIds<T: Config> =
-		StorageMap<Pallet<T>, Blake2_128Concat, <T as Config>::RelayChainAccountId, ()>;
-
-	#[frame_support::storage_alias]
-	type InitRelayBlock<T: Config> =
-		StorageValue<Pallet<T>, <T as Config>::VestingBlockNumber, ValueQuery>;
-
-	#[frame_support::storage_alias]
-	type EndRelayBlock<T: Config> =
-		StorageValue<Pallet<T>, <T as Config>::VestingBlockNumber, ValueQuery>;
-
-	/// Trivial migration which makes the roles of each pool optional.
-	///
-	/// Note: The depositor is not optional since he can never change.
 	pub struct MigrateToV1<T>(sp_std::marker::PhantomData<T>);
 	impl<T: Config> OnRuntimeUpgrade for MigrateToV1<T> {
 		fn on_runtime_upgrade() -> Weight {
 			let current = Pallet::<T>::current_storage_version();
 			let onchain = Pallet::<T>::on_chain_storage_version();
-			const DEFAULT_CROWDLOAN_ID: u32 = 0u32;
-			let mut counter: u64 = 0;
 
 			if current == 1 && onchain == 0 {
 				log!(
@@ -76,53 +38,8 @@ pub mod v1 {
 					onchain
 				);
 
-				for (key, value) in v1::UnassociatedContributions::<T>::drain() {
-					counter += 1;
-					crate::pallet::UnassociatedContributions::<T>::insert(
-						DEFAULT_CROWDLOAN_ID,
-						key,
-						value,
-					);
-				}
-
-				for (key, value) in v1::AccountsPayable::<T>::drain() {
-					counter += 1;
-					crate::pallet::AccountsPayable::<T>::insert(DEFAULT_CROWDLOAN_ID, key, value);
-				}
-
-				let mut writes_counter: u64 = 0;
-				// When 1st crowdloan was initialized with all the data (relay & local ids set)
-				// The `ClaimedRelayChainIds` storage entry is only used by `associate_native_identity`
-				// when someone (root/council) would like to associate unassociated rewards with
-				// particular account, so its obsolete to migrate this info. And because of
-				// extra cost of migration of `ClaimedRelayChainIds` we would be exceeding the block limits
-				// lets just ignore it
-				for (_key, _value) in v1::ClaimedRelayChainIds::<T>::drain() {
-					writes_counter += 1;
-				}
-
-				crate::pallet::CrowdloanAllocation::<T>::insert(
-					DEFAULT_CROWDLOAN_ID,
-					v1::CrowdloanAllocation::<T>::take(),
-				);
-
-				crate::pallet::InitializedRewardAmount::<T>::insert(
-					DEFAULT_CROWDLOAN_ID,
-					v1::InitializedRewardAmount::<T>::take(),
-				);
-
-				crate::pallet::TotalContributors::<T>::insert(
-					DEFAULT_CROWDLOAN_ID,
-					v1::TotalContributors::<T>::take(),
-				);
-
-				let init = v1::InitRelayBlock::<T>::take();
-				let end = v1::EndRelayBlock::<T>::take();
-				crate::pallet::CrowdloanPeriod::<T>::insert(DEFAULT_CROWDLOAN_ID, (init, end));
-
-				log!(info, "Migrated entries: {}", counter);
 				current.put::<Pallet<T>>();
-				T::DbWeight::get().reads_writes(counter, writes_counter + counter + 2 + 1)
+				T::DbWeight::get().reads_writes(2, 1)
 			} else {
 				log!(
 					info,
@@ -130,36 +47,6 @@ pub mod v1 {
 				);
 				T::DbWeight::get().reads(2)
 			}
-		}
-
-		#[cfg(feature = "try-runtime")]
-		fn pre_upgrade() -> Result<sp_std::vec::Vec<u8>, TryRuntimeError> {
-			log!(info, "Crowdloan::pre_upgrade start");
-			let onchain = Pallet::<T>::on_chain_storage_version();
-			let current = Pallet::<T>::current_storage_version();
-			if current == 1 && onchain == 0 {
-				assert_eq!(Pallet::<T>::on_chain_storage_version(), 0);
-				assert!(v1::AccountsPayable::<T>::iter().count() > 0);
-				assert!(crate::pallet::CrowdloanPeriod::<T>::iter().count() == 0);
-			}
-			log!(info, "Crowdloan::pre_upgrade finished");
-			Ok(Default::default())
-		}
-
-		#[cfg(feature = "try-runtime")]
-		fn post_upgrade(_state: sp_std::vec::Vec<u8>) -> Result<(), TryRuntimeError> {
-			const DEFAULT_CROWDLOAN_ID: u32 = 0u32;
-			log!(info, "Crowdlon::post_upgrade start");
-			assert_eq!(Pallet::<T>::current_storage_version(), 1);
-
-			for (key, key2, value) in crate::pallet::AccountsPayable::<T>::iter() {
-				assert_eq!(key, DEFAULT_CROWDLOAN_ID);
-			}
-
-			assert!(crate::AccountsPayable::<T>::iter().count() > 0);
-
-			log!(info, "Crowdlon::post_upgrade finished");
-			Ok(())
 		}
 	}
 }
